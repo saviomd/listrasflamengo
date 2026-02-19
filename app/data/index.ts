ad49d7f2f2d97355f2e1d312c23e9aaa548d501e
Sávio Mendes <saviomd@users.noreply.github.com>
2026-02-19 00:12:51 -0300

    Update basename, manifest data

--- app/data/index.ts
+++ app/data/index.ts
@@ -1,2 +1,3 @@
 export { default as chat } from "./chat";
 export { default as jerseys } from "./jerseys";
+export { default as manifest } from "./manifest";
--- app/data/manifest.ts
+++ app/data/manifest.ts
@@ -2,13 +2,13 @@ const color = "#b71c1c";
 const iconSizes = ["192", "512"];
 const url = `${import.meta.env.PROD ? "https://saviomd.com" : ""}${import.meta.env.BASE_URL}`;
 
-export const manifest = {
+const manifest = {
   background_color: color,
   description: "Evolução ano a ano das listras na camisa principal do Flamengo",
   display: "standalone",
   icons: iconSizes.map((size) => ({
     sizes: `${size}x${size}`,
-    src: `${url}img/icon-${size}.png`,
+    src: `${url}/img/icon-${size}.png`,
     type: "image/png",
   })),
   name: "Listras do Flamengo",
@@ -17,10 +17,4 @@ export const manifest = {
   theme_color: color,
 };
 
-export function loader() {
-  return Response.json(manifest, {
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-  });
-}
+export default manifest;
